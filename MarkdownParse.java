282e01b0b723b10ba788b735a1a49a3cdc017f77
Sri @ Yoda <smissula@ucsd.edu>
2022-01-20 07:34:38 +0530

    Added breaking test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,6 +10,7 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
+        System.out.print(currentIndex + " ");
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
@@ -17,7 +18,9 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.print(currentIndex + " ");
         }
+        System.out.println();
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
---
+++ breaking-test.md
@@ -0,0 +1,3 @@
+This is a new test: we're testing syntax errors now!
+
+[False link](https://www.github.com/
