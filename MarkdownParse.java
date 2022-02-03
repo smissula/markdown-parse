a12a40f6355247717f87f6cbd59455dfdac9c989
Sri @ Yoda <smissula@ucsd.edu>
2022-02-02 17:38:39 -0800

    Updated logic to match team's

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,48 +18,27 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             // first closing parenthesis 
             int closeParen = markdown.indexOf(")", openParen);
-            // index of end of file
-            int endOfFile = markdown.length();
-            // index of end of line
-            int endOfLine = markdown.indexOf("\n", openParen);
-
-            // test breaker for images
-            if (nextOpenBracket > 0 &&
-                markdown.substring(nextOpenBracket-1, nextOpenBracket).equals("!")) {
-                currentIndex = nextOpenBracket + 1;
-                continue;
-            }
-
-            if (closeParen == -1) {
-                if (endOfLine == -1) {
-                    toReturn.add(markdown.substring(openParen+1, endOfFile));
-                    currentIndex = endOfFile;
-                    // System.out.println("1: New current idx: " + currentIndex);
-                }
-                else {
-                    toReturn.add(markdown.substring(openParen+1, 
-                                Math.min(endOfLine, endOfFile)));
-                    currentIndex = Math.min(endOfLine, endOfFile) + 1;
-                    // System.out.println("2: New current idx: " + currentIndex);
-                }
-            }
-            else if (endOfLine == -1) {
-                toReturn.add(markdown.substring(openParen+1, 
-                            Math.min(closeParen, endOfFile)));
-                currentIndex = Math.min(closeParen, endOfFile) + 1;
-                // System.out.println("3: New current idx: " + currentIndex);
-            }
-            else {
-                toReturn.add(markdown.substring(openParen+1, 
-                        Math.min(endOfLine, Math.min(endOfFile, closeParen))));
-                currentIndex = Math.min(endOfLine,
-                    Math.min(endOfFile, closeParen)) + 1;
-                // System.out.println("4: New current idx: " + currentIndex);
+            if (nextOpenBracket != 0 && markdown.substring(
+                        nextOpenBracket -1, nextOpenBracket).equals("!")) {
+                currentIndex = closeParen + 1;
             }
             
-            System.out.print(currentIndex + " ");
+            else {
+                if (closeParen > -1) {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                    currentIndex = closeParen + 1;
+                }
+                else {
+                    currentIndex = markdown.indexOf("\n", openParen) + 1;
+                }
+                if (currentIndex == 0) {
+                    break;
+                }
+            }
+            
+            // System.out.print(currentIndex + " ");
         }
-        System.out.println();
+        // System.out.println();
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
